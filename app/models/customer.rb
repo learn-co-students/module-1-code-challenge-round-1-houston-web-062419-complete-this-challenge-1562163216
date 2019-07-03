be1e29f14683bea0b78a26e8eb02c99f5961c9c4
github username <github email address>
2019-07-03 10:25:40 -0500

    thank you kindly

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,66 @@
 class Customer
   attr_reader :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def reviews    
+    Review.all.select do |a|
+      a.customer == self
+    end
+  end
+
+  def num_reviews
+    num =Review.all.select do |a|
+      a.customer == self
+    end
+    num.length
+  end
+
+  def restaurants
+    reviews.map do |a|
+      a.restaurant.name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    array_name = []
+    self.all.select do |a|
+      if a.first_name.downcase.include? name.downcase
+          array_name << a.first_name
+      end
+    end
+    array_name
+  end
+  
+
+  def self.all_names(name)
+    array_allname = []
+    self.all.select do |a|
+      if a.first_name.downcase.include? name.downcase
+          array_allname << "#{a.first_name} #{a.last_name}"
+      end
+    end
+    array_allname
+  end
+
+  
+  
+  
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,28 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+
+c1 = Customer.new("First1", "Last1")
+c2 = Customer.new("First2", "Last2")
+c3 = Customer.new("First3", "Last3")
+
+
+r1 = Restaurant.new("Restaurant1")
+r2 = Restaurant.new("Restaurant2")
+r3 = Restaurant.new("Restaurant3")
+
+re1 = Review.new(r1, c1, "Content111", 5)
+re2 = Review.new(r1, c2, "Content2", 4)
+re3 = Review.new(r2, c1, "Content3", 3)
+re4 = Review.new(r2, c2, "Content4", 2)
+re5 = Review.new(r3, c1, "Content5", 5)
+re6 = Review.new(r3, c2, "Content6", 5)
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
