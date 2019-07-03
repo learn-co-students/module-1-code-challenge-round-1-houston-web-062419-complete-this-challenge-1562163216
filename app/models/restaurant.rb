872fd388109b3a8506cb1d85144771ad3a8669f6
Varvara Babkova <varvara.munday@gmail.com>
2019-07-03 09:59:02 -0500

    kinda done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,47 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+
   end
+
+   def self.all
+  	@@all
+  end
+
+#    `Restaurant#customers`
+#   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+# - `Restaurant#longest_review`
+#   - returns the longest review content for a give
+
+	def customers
+		reviews.map {|r| r.customer}.uniq
+	end
+
+	def reviews
+		Review.all.select{|r| r.restaurant == self}
+	end
+
+	def average_star_rating
+		reviews.sum {|r| r.rating}.to_f / reviews.size
+	end
+
+	def longest_review
+		reviews.max {|r1, r2| r1.content.length <=> r2.content.length}
+	end
+
+	# `Restaurant.find_by_name(name)`
+ #  - given a string of restaurant name, returns the first restaurant that matches
+
+ 	def self.find_by_name(name)
+ 		all.find {|r| r.name == name}
+ 	end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
-  
+
+	@@all = []
+	attr_accessor :customer, :restaurant, :content, :rating
+
+	def initialize(customer, restaurant, content, rating)
+		@customer = customer
+		@restaurant = restaurant
+		@content = content
+		@rating = rating
+
+		@@all << self
+
+	end
+
+ 	def self.all
+  		@@all
+  	end
+
+#   	- `Review#customer`
+#   - returns the customer object for that given review
+#   - Once a review is created, I should not be able to change the author
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, I should not be able to change the restaurant
+# - `Review#rating`
+#   - returns the star rating for a restaurant. This should be an integer from 1-5
+# - `Review#content`
+#   - returns the review content, as a string, for a particular review
+
+	
 end
 
