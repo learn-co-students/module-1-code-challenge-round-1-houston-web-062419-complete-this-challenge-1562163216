e9fa1d6141a4eba78e6250d1f7d95baa5687d000
abideen11 <abideenkareem17@gmail.com>
2019-07-03 10:25:19 -0500

    finish

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all 
+    @@all 
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self 
+    end
+  end 
+
+  def customers 
+    reviews.uniq
+  end
+
+  def total 
+    total  = 0 
+    reviews.each do |review|
+      total += review.star
+    end 
+    total
+  end
+
+  def average_star_rating
+    total/reviews.restaurant.length 
+  end 
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
+
+    attr_accessor :customer, :restaurant 
+    @@all = []
+
+    def initialize(customer,restaurant,star)
+        @customer = customer
+        @restaurant = restaurant
+        @star = star
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
 end
 
